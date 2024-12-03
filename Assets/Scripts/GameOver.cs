dfab10c869abfcc45f8fc191c3cf6c4121112359
snailSlayer420 <shuckydarns81@gmail.com>
2024-12-03 17:56:57 -0500

    Started Project

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    
+    public GameObject platformGenerator;
+ 
+
+    public GameObject GameOverScreen;
+    public Text gameOverScoreText;
+   
+
+    void Start()
+    {
+      
+
+        GameOverScreen.SetActive(false);
+    }
+    public void gameOver()
+    {
+       
+
+        GameOverScreen.SetActive(true);
+        //gameOverScoreText.text = Pickups.score.ToString();
+    }
+}
